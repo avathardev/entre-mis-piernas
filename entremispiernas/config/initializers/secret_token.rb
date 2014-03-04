3909461ee0a5f767010894ef29b3ab2b30baadba
johnnyleeothon <johnny.lee.othon@outlook.com>
2014-03-03 20:36:38 -0600

    debería tener un inicio
    :)

---
+++ entremispiernas/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Entremispiernas::Application.config.secret_key_base = '0ae9ee22fa539dcc924b0e022ce129da6cb92e368f39cb3c29c3fd91f4e8f95d7d59e2577695ca81ffc0282c4e6444501bde61b3e83c1dd0ba532d511160ec44'
